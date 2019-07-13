a29c058a2373dcf24a0f9391876e88c8fa86dcf7
Nikolay Lysenko <nikolay.lysenko.1992@gmail.com>
2019-07-13 16:00:32 +0300

    chore: Update version while fixing PyPI uploading issue

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(os.path.join(current_dir, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='sinethesizer',
-    version='0.2.0',
+    version='0.2.1',
     description=description,
     long_description=long_description,
     long_description_content_type='text/markdown',
