06f4effa4f88d6a084fd3ef7fc66f501f6c38996
Nikolay Lysenko <nikolay.lysenko.1992@gmail.com>
2019-07-13 17:40:28 +0300

    fix: Include configuration file to PyPI package

--- setup.py
+++ setup.py
@@ -30,6 +30,9 @@ setup(
     license='MIT',
     keywords='synthesizer analog_synthesizer additive_synthesis music timbre',
     packages=find_packages(exclude=['tests', 'docs']),
+    data_files=[
+        ('sinethesizer', os.path.join('sinethesizer', 'default_config.yml'))
+    ],
     python_requires='>=3.6',
     install_requires=['numpy', 'PyYAML', 'scipy']
 )
