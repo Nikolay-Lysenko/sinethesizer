6a6df22068e3f16f1ca0ad2df05497c4ac78f734
Nikolay Lysenko <nikolay.lysenko.1992@gmail.com>
2019-07-14 00:17:27 +0300

    fix: Make config file available at runtime after sdist installation

--- sinethesizer/__main__.py
+++ sinethesizer/__main__.py
@@ -6,7 +6,7 @@ Author: Nikolay Lysenko
 
 
 import argparse
-import os
+from pkg_resources import resource_string
 
 import yaml
 
@@ -47,27 +47,27 @@ def parse_cli_args() -> argparse.Namespace:
     parser.set_defaults(safe=False)
 
     cli_args = parser.parse_args()
-    if cli_args.config_path is None:
-        cli_args.config_path = os.path.join(
-            os.path.dirname(__file__), 'default_config.yml'
-        )
-    if cli_args.presets_path is None:
-        cli_args.presets_path = os.path.join(
-            os.path.dirname(__file__), '..', 'presets', 'demo.yml'
-        )
     return cli_args
 
 
 def main():
     """Run all necessary code."""
     cli_args = parse_cli_args()
+
     timbres_registry = create_timbres_registry(cli_args.presets_path)
     if cli_args.safe:
         for _, timbre_spec in timbres_registry.items():
             validate_timbre_spec(timbre_spec)
-    with open(cli_args.config_path) as config_file:
-        settings = yaml.safe_load(config_file)
+
+    if cli_args.config_path is None:
+        settings = yaml.safe_load(
+            resource_string(__name__, 'default_config.yml')
+        )
+    else:
+        with open(cli_args.config_path) as config_file:
+            settings = yaml.safe_load(config_file)
     settings['timbres_registry'] = timbres_registry
+
     timeline = convert_tsv_to_timeline(cli_args.input_path, settings)
     write_timeline_to_wav(
         cli_args.output_path, timeline, settings['frame_rate']
