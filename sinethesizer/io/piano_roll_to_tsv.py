fb615a1d84a06f0fa65201ecaafe9a421c6e105c
Nikolay Lysenko <nikolay.lysenko.1992@gmail.com>
2019-09-04 22:36:01 +0300

    refac: Improve code maintainability

--- sinethesizer/io/piano_roll_to_tsv.py
+++ sinethesizer/io/piano_roll_to_tsv.py
@@ -13,7 +13,7 @@ Author: Nikolay Lysenko
 """
 
 
-from typing import List
+from typing import Any, List, Tuple
 
 import numpy as np
 from sinethesizer.io.utils import get_list_of_notes
@@ -32,11 +32,42 @@ def find_involved_notes(lowest_note: str, n_notes: int) -> List[str]:
     """
     all_notes = get_list_of_notes()
     position = 0
-    for position, note in enumerate(all_notes):
+    for position, note in enumerate(all_notes):  # pragma: no branch
         if note == lowest_note:
             break
     involved_notes = all_notes[position:(position + n_notes)]
     return involved_notes
+
+
+def add_events_with_note(
+        events: List[Tuple[Any, ...]], row: np.ndarray, note: str,
+        row_number: int, step_in_seconds: float
+) -> List[Tuple[Any, ...]]:
+    """
+    Add all events with a particular note to the list of events.
+
+    :param events:
+        list of events to be extended
+    :param row:
+        timeline of playing the note
+    :param note:
+        letter notation for the note
+    :param row_number:
+        number of `row` on its piano roll
+    :param step_in_seconds:
+        duration of one time step of piano roll in seconds
+    :return:
+        extended list of events
+    """
+    start_time = None
+    for j in range(len(row) + 1):
+        if j < len(row) and row[j] == 1 and start_time is None:
+            start_time = j * step_in_seconds
+        if (j == len(row) or row[j] == 0) and start_time is not None:
+            duration = j * step_in_seconds - start_time
+            events.append((start_time, duration, note, -row_number))
+            start_time = None
+    return events
 
 
 def convert_roll_to_events(
@@ -64,24 +95,17 @@ def convert_roll_to_events(
     :return:
         list of events
     """
-    results = []
+    events = []
     for i in range(roll.shape[0]):
         row = roll[i, :]
-        frequency = roll_notes[::-1][i]
-        start_time = None
-        for j in range(roll.shape[1] + 1):
-            if j < roll.shape[1] and row[j] == 1 and start_time is None:
-                start_time = j * step_in_seconds
-            if (j == roll.shape[1] or row[j] == 0) and start_time is not None:
-                duration = j * step_in_seconds - start_time
-                results.append((
-                    timbre, start_time, duration, frequency, volume,
-                    location, effects, -i  # `i` is needed for sorting.
-                ))
-                start_time = None
-    results = sorted(results, key=lambda x: (x[1], x[-1], x[2]))
-    results = ['\t'.join([str(y) for y in x[:-1]]) for x in results]
-    return results
+        note = roll_notes[::-1][i]
+        events = add_events_with_note(events, row, note, i, step_in_seconds)
+    events = sorted(events, key=lambda x: (x[0], x[3], x[1]))
+    events = [
+        f"{timbre}\t{x[0]}\t{x[1]}\t{x[2]}\t{volume}\t{location}\t{effects}"
+        for x in events
+    ]
+    return events
 
 
 def write_roll_to_tsv_file(
