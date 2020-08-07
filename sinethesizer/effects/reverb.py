e3197e64a60b9e3ccf3de066a57e384c75985139
Nikolay Lysenko <nikolay.lysenko.1992@gmail.com>
2020-08-07 23:15:02 +0300

    refac: Split long function into multiple smaller functions

--- sinethesizer/effects/reverb.py
+++ sinethesizer/effects/reverb.py
@@ -8,10 +8,98 @@ Author: Nikolay Lysenko
 import random
 import warnings
 from math import ceil
-from typing import Any, Dict, Optional
+from typing import Any, Dict, List, Optional
 
 import numpy as np
 from scipy.signal import convolve
+
+
+def validate_inputs(
+        decay_duration: float, n_early_reflections: int,
+        early_reflections_delay: float, diffusion_delay_factor: float
+) -> None:
+    """
+    Validate arguments passed to `apply_reverb` function.
+
+    :param decay_duration:
+        total time (in seconds) since the first reflection until the last one
+    :param n_early_reflections:
+        number of equally spaced reflections
+    :param early_reflections_delay:
+        time (in seconds) between successive early reflections
+    :param diffusion_delay_factor:
+        exponential decay factor for time between successive late reflections
+    :return:
+        None
+    """
+    early_span = (n_early_reflections - 1) * early_reflections_delay
+    late_reflections_duration = decay_duration - early_span
+    if late_reflections_duration < 0:
+        raise ValueError("Early reflections last more than all reflections.")
+    if diffusion_delay_factor < 0:
+        raise ValueError("Diffusion delay factor can not be negative.")
+    max_late_reflections_duration = (
+        # Right multiplier is sum of geometric progression: a + a^2 + a^3 + ...
+        early_reflections_delay * (1 / (1 - diffusion_delay_factor) - 1)
+        if diffusion_delay_factor < 1 else np.inf
+    )
+    padding = 0.01
+    if max_late_reflections_duration < late_reflections_duration + padding:
+        raise ValueError(
+            "Inconsistent parameters. "
+            "Increase `diffusion_delay_factor` or decrease `decay_duration`."
+        )
+    if diffusion_delay_factor > 1:  # pragma: no cover
+        warnings.warn(
+            "If `diffusion_delay_factor` > 1, reverb is not realistic.",
+            UserWarning
+        )
+
+
+def find_reflection_times(
+        ir_duration_in_seconds: float,
+        first_reflection_delay: float,
+        n_early_reflections: int,
+        early_reflections_delay: float,
+        diffusion_delay_factor: float,
+        diffusion_delay_random_range: float
+) -> List[float]:
+    """
+    Find delays with which reflected waves arrive to a listener.
+
+    :param ir_duration_in_seconds:
+        duration of impulse response wave (in seconds)
+    :param first_reflection_delay:
+        time (in seconds) between original sound and its first reflection
+    :param n_early_reflections:
+        number of equally spaced reflections
+    :param early_reflections_delay:
+        time (in seconds) between successive early reflections
+    :param diffusion_delay_factor:
+        exponential decay factor for time between successive late reflections
+    :param diffusion_delay_random_range:
+        relative range where time between successive late reflections
+        can randomly vary
+    :return:
+        delays with which reflected waves arrive to a listener
+    """
+    reflection_times = [first_reflection_delay]
+    for i in range(n_early_reflections - 1):
+        next_reflection_time = reflection_times[-1] + early_reflections_delay
+        reflection_times.append(next_reflection_time)
+    delay_without_randomness = early_reflections_delay
+    while True:
+        delay_without_randomness *= diffusion_delay_factor
+        random_number = random.uniform(
+            -diffusion_delay_random_range, diffusion_delay_random_range
+        )
+        delay = delay_without_randomness * (1 + random_number)
+        reflection_time = reflection_times[-1] + delay
+        if reflection_time < ir_duration_in_seconds:
+            reflection_times.append(reflection_time)
+        else:
+            break
+    return reflection_times
 
 
 def apply_reverb(
@@ -65,29 +153,11 @@ def apply_reverb(
     :return:
         reverberated sound
     """
-    random.seed(random_seed)
-    early_span = (n_early_reflections - 1) * early_reflections_delay
-    late_reflections_duration = decay_duration - early_span
-    if late_reflections_duration < 0:
-        raise ValueError("Early reflections last more than all reflections.")
-    if diffusion_delay_factor < 0:
-        raise ValueError("Diffusion delay factor can not be negative.")
-    max_late_reflections_duration = (
-        # Right multiplier is sum of geometric progression: a + a^2 + a^3 + ...
-        early_reflections_delay * (1 / (1 - diffusion_delay_factor) - 1)
-        if diffusion_delay_factor < 1 else np.inf
+    validate_inputs(
+        decay_duration, n_early_reflections, early_reflections_delay,
+        diffusion_delay_factor
     )
-    padding = 0.01
-    if max_late_reflections_duration < late_reflections_duration + padding:
-        raise ValueError(
-            "Inconsistent parameters. "
-            "Increase `diffusion_delay_factor` or decrease `decay_duration`."
-        )
-    if diffusion_delay_factor > 1:  # pragma: no cover
-        warnings.warn(
-            "If `diffusion_delay_factor` > 1, reverb is not realistic.",
-            UserWarning
-        )
+    random.seed(random_seed)
 
     frame_rate = sound_info['frame_rate']
     ir_duration_in_seconds = first_reflection_delay + decay_duration
@@ -95,23 +165,11 @@ def apply_reverb(
     impulse_response = np.zeros(ir_duration_in_frames)
     impulse_response[0] = original_sound_gain
 
-    reflection_times = [first_reflection_delay]
-    for i in range(n_early_reflections - 1):
-        next_reflection_time = reflection_times[-1] + early_reflections_delay
-        reflection_times.append(next_reflection_time)
-    delay_without_randomness = early_reflections_delay
-    while True:
-        delay_without_randomness *= diffusion_delay_factor
-        random_number = random.uniform(
-            -diffusion_delay_random_range, diffusion_delay_random_range
-        )
-        delay = delay_without_randomness * (1 + random_number)
-        reflection_time = reflection_times[-1] + delay
-        if reflection_time < ir_duration_in_seconds:
-            reflection_times.append(reflection_time)
-        else:
-            break
-
+    reflection_times = find_reflection_times(
+        ir_duration_in_seconds, first_reflection_delay,
+        n_early_reflections, early_reflections_delay,
+        diffusion_delay_factor, diffusion_delay_random_range
+    )
     for reflection_time in reflection_times:
         index = min(
             int(round(frame_rate * reflection_time)),
