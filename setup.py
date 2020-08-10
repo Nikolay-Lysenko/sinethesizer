4393e5b50b4595f39d1e4f4316dc4d1e4e991dfc
Nikolay Lysenko <nikolay.lysenko.1992@gmail.com>
2020-08-10 21:54:46 +0300

    chore: Upgrade package version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(os.path.join(current_dir, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='sinethesizer',
-    version='0.2.11',
+    version='0.3.0',
     description=description,
     long_description=long_description,
     long_description_content_type='text/markdown',
