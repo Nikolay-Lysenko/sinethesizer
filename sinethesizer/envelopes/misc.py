c1654fed80df204b35c325b931976b656e78ce27
Nikolay Lysenko <nikolay.lysenko.1992@gmail.com>
2020-10-12 21:07:51 +0300

    feat: Add envelope proportional to frequency

--- sinethesizer/envelopes/misc.py
+++ sinethesizer/envelopes/misc.py
@@ -26,3 +26,25 @@ def create_constant_envelope(
         constant envelope
     """
     return value * np.ones(ceil(event.duration * event.frame_rate))
+
+
+def create_proportional_to_frequency_constant_envelope(
+        event: 'sinethesizer.synth.core.Event', ratio: float
+) -> np.ndarray:
+    """
+    Create constant envelope with value linearly depending on frequency.
+
+    This envelope can be used for adding vibrato with frequency or phase
+    modulation.
+
+    :param event:
+        parameters of sound event for which this function is called;
+        this argument provides information about duration, frame rate,
+        and frequency
+    :param ratio:
+        ratio of envelope value to frequency (in Hz)
+    :return:
+        frequency-dependent constant envelope
+    """
+    value = ratio * event.frequency
+    return value * np.ones(ceil(event.duration * event.frame_rate))
